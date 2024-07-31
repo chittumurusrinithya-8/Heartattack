98c91daa881d27baa3db7310ad64a7f226df3b22
chittumurusrinithya-8 <chittumurusrinithya@gmail.com>
2024-07-31 11:17:06 +0530

    Update heartattack.py

--- heartattack.py
+++ heartattack.py
@@ -4,7 +4,6 @@ import pickle
 
 trained_model = pickle.load(open('trained_model.sav','rb'))
 
- 
 # Title
 st.title("Heart Attack Precdictor")
 age = st.text_input("Enter Your age", "0")
@@ -19,19 +18,29 @@ chestpain = st.selectbox("Chest pain type:",["Typical Angina","Atypical Angina",
 chestpain = chp[chestpain]
 trestbps = st.text_input("Enter your Resting blood pressure (in mm Hg)","0")
 chol = st.text_input("Enter your  Cholestoral in mg/dl fetched via BMI sensor","0")
-fbs = st.selectbox("fasting blood sugar:",['1 = True','0 = False'])
-restecg = st.selectbox("Ecg Result:",['0 = Normal','1 = ST-T wave normality','2 = Left ventricular hypertrophy'])
+fbs = st.selectbox("fasting blood sugar:",['True','False'])
+if fbs == "True":
+    fbs = 1
+else:
+    fbs = 0
+ecgd = {"Normal":0,"ST-T wave normality":1,"Left ventricular hypertrophy":2}
+restecg = st.selectbox("Ecg Result:",['Normal','ST-T wave normality','Left ventricular hypertrophy'])
+restecg = ecgd[restecg]
 thalach =st.text_input("Maximum heart rate achieved","0")
 oldpeak = st.text_input("Old peak value calculated from ecg")
 slope = st.text_input("slope value calculated from ecg")
 thall = st.selectbox("Thalium Stress Test result:",['0','1','2','3'])
-exng = st.selectbox("Exercise induced angina:",['1 = Yes','0 = No'])
+exng = st.selectbox("Exercise induced angina:",['Yes','No'])
+if exng == "Yes":
+    exng = 1
+else:
+    exng = 0
 caa = st.selectbox("number of major vessels:",['0','1','2','3'])
 
 
 if st.button('Heart Disease Test Result'):
 
-    user_input = [age,trestbps,chol,thalach, oldpeak, slope,caa, thall,thalach, chestpain, slope,caa, gender]
+    user_input = [age,gender,chestpain,trestbps,chol,fbs,restecg,thalach,oldpeak,slope,thall,exng,caa]
 
     user_input = [float(x) for x in user_input]
 
